758b594d88255057a162df4e40e2801f8430f401
naglem3 <marcel.naegle@contractors.roche.com>
2025-02-28 11:08:44 +0100

    Add logger

--- src/main/java/com/example/EventMessagePublisher/EventMessagePublisherApplication.java
+++ src/main/java/com/example/EventMessagePublisher/EventMessagePublisherApplication.java
@@ -1,11 +1,13 @@
 package com.example.EventMessagePublisher;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class EventMessagePublisherApplication {
 
 	@Value("${mqtt.publish.topic}")
